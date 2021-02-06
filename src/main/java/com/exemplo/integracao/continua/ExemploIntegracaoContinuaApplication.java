09be2fcf98fef32e0ebf096258059c484a395d63
FernandoUnix <01fernando.ti@gmail.com>
2021-02-06 19:04:06 -0300

    adicionado runtime version

---
+++ src/main/java/com/exemplo/integracao/continua/ExemploIntegracaoContinuaApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.integracao.continua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploIntegracaoContinuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploIntegracaoContinuaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exemplo/integracao/continua/ExemploIntegracaoContinuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.integracao.continua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploIntegracaoContinuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
