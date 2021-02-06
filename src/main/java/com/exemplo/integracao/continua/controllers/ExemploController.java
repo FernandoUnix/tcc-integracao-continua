6e86c836714f537cc16affd056de3eaa3c12a99d
FernandoUnix <01fernando.ti@gmail.com>
2021-02-06 20:53:29 -0300

    Versão 2

--- src/main/java/com/exemplo/integracao/continua/controllers/ExemploController.java
+++ src/main/java/com/exemplo/integracao/continua/controllers/ExemploController.java
@@ -10,7 +10,7 @@ public class ExemploController {
     @GetMapping("/index")
     public String index(Model model) {
 
-        model.addAttribute("versao", "Versão 1");
+        model.addAttribute("versao", "Versão 2");
         return "index";
     }
 }
